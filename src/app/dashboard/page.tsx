2ab7dd9f763d0d4b55c5014258091042bb725a1f
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2024-02-18 01:29:21 +0800

    [feature/note-ui-openai-api] Create note ui, and add api for openai generate image prompt and create notebook api

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,7 @@
+import CreateNoteDialog from "@/components/CreateNoteDialog";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { UserButton } from "@clerk/nextjs";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React from "react";
@@ -13,12 +16,29 @@ const DashboardPage = (props: Props) => {
         <div className="flex- justify-between items-center md:flex-row flex-col">
           <div className="flex items-center">
             <Link href="/">
-              <Button className="bg-green-600">
+              <Button className="bg-green-600" size="sm">
                 {" "}
                 <ArrowLeft className="mr-1 w-4 h-4" />
                 Back
               </Button>
             </Link>
+            <div className="w-4"></div>
+            <h1 className="text-3xl font-bold text-gray-900">My Notes</h1>
+            <div className="w-4"></div>
+            <UserButton />
+          </div>
+          <div className="h-8"></div>
+          <Separator />
+          <div className="h-8"></div>
+          {/* list all the notes */}
+          {/* TODO conditionally rendered */}
+          <div className="text-center">
+            <h2 className="text-xl text-gray-500"> You have no notes yet.</h2>
+          </div>
+
+          {/* display all the notes */}
+          <div className="grid sm:grid-cols-3 md:grid-cols-5 grid-cols-1 gap-3">
+            <CreateNoteDialog />
           </div>
         </div>
       </div>
