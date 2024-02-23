62a5218a5a0a67ff47639bc0b561cf55fe2ed775
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2024-02-23 18:32:52 +0800

    [feature/upload-firebase-delete-note] Finished functionality for upload to firebase to get permenant image url to store into neon db, and image into firebase storage. Finished development for delete button and delete note

--- src/app/notebook/[noteId]/page.tsx
+++ src/app/notebook/[noteId]/page.tsx
@@ -1,3 +1,4 @@
+import DeleteButton from "@/components/DeleteButton";
 import TipTapEditor from "@/components/TipTapEditor";
 import { Button } from "@/components/ui/button";
 import { clerk } from "@/lib/clerk-server";
@@ -16,7 +17,7 @@ type Props = {
 };
 
 const NotebookPage = async ({ params: { noteId } }: Props) => {
-  const { userId } = auth();
+  const { userId } = await auth();
   if (!userId) {
     return redirect("/dashboard");
   }
@@ -33,7 +34,7 @@ const NotebookPage = async ({ params: { noteId } }: Props) => {
 
   return (
     <div className="min-h-screen grainy p-8">
-      <div className="max-4xl mx-auto">
+      <div className="max-w-4xl mx-auto">
         <div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
           <Link href="/dashboard">
             <Button className="bg-green-600" size="sm">
@@ -44,10 +45,13 @@ const NotebookPage = async ({ params: { noteId } }: Props) => {
           <span className="font-semibold">
             {user.firstName} {user.lastName}
           </span>
-          <span className="inline-block mx-1"></span>
-          <span className="text-stone-500 font-semibold">/ {note.name}</span>
-          <div className="ml-auto">DELETE BUTTON</div>
+          <span className="inline-block mx-1">/</span>
+          <span className="text-stone-500 font-semibold">{note.name}</span>
+          <div className="ml-auto">
+            <DeleteButton noteId={note.id} />
+          </div>
         </div>
+
         <div className="h-4"></div>
         <div className="border-stone-200 shadow-xl border rounded-lg px-16 py-8 w-full">
           <TipTapEditor note={note} />
